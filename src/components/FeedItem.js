4f595d282e4c46886dbae2b201a253fc66aa39ac
Clarence Chan <clarencechaan@icloud.com>
2022-05-31 15:49:08 -0400

    implement transition/animation for feeditem

--- src/components/FeedItem.js
+++ src/components/FeedItem.js
@@ -1,28 +1,51 @@
 import "../styles/FeedItem.css";
-import { ChatCircle } from "phosphor-react";
+import { ChatCircle, Link, MagnifyingGlassPlus } from "phosphor-react";
 
 function FeedItem() {
   return (
     <div className="feed-item">
       <div className="img-container">
         <img src="https://i.imgur.com/osBpkIK.jpg" alt="" />
-        <div className="blue-overlay"></div>
+        <div className="blue-overlay">
+          <a href="##" className="post-link">
+            <div className="badge">
+              <Link size={24} />
+            </div>
+          </a>
+          <button className="full-size">
+            <div className="badge">
+              <MagnifyingGlassPlus size={24} />
+            </div>
+          </button>
+        </div>
       </div>
       <div className="content-preview">
         <div className="by-line">
-          <span className="author">by shufflehound</span>
-          <span className="date">November 23, 2016</span>
+          by{" "}
+          <a href="##" className="author">
+            shufflehound
+          </a>
+          <a href="##" className="date">
+            November 23, 2016
+          </a>
         </div>
-        <div className="title">Trip that you’ll never ever forget</div>
+        <a href="##" className="title">
+          Trip that you’ll never ever forget
+        </a>
         <div className="body">
           Quisque dictum eros nisl, a maximus massa accumsan non. Aliquam erat
-          volutpat. Quisque at finibus dui. Praesent…
+          volutpat. Quisque at finibus dui. Quisque dictum eros nisl, a maximus
+          massa accumsan non. Aliquam erat volutpat. Quisque at finibus
+          dui.Quisque dictum eros nisl, a maximus massa accumsan non. Aliquam
+          erat volutpat. Quisque at finibus dui.Quisque dictum eros nisl, a
+          maximus massa accumsan non. Aliquam erat volutpat. Quisque at finibus
+          dui. Praesent…
         </div>
         <div className="full-name">RICARDO VALENTINE</div>
-        <div className="comment-badge">
+        <a href="##" className="comment-badge">
           <ChatCircle size={18} />
           <div className="count">0</div>
-        </div>
+        </a>
       </div>
     </div>
   );
