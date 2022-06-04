47b0bdca42ef82209ccc9b4639fed21f81027474
Clarence Chan <clarencechaan@icloud.com>
2022-06-04 15:42:13 -0400

    restyle comments section + footer

--- src/components/CommentsFeed.js
+++ src/components/CommentsFeed.js
@@ -3,9 +3,9 @@ import Comment from "./Comment";
 
 function CommentsFeed() {
   return (
-    <div className="comments-feed-container">
+    <div id="comments" className="comments-feed-container">
       <h3 className="comments-title">5 comments</h3>
-      <div className="comments-feed" id="comments">
+      <div className="comments-feed">
         <Comment />
         <Comment />
         <Comment />
